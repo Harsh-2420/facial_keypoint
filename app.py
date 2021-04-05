1a6b9a5c42765d6f83c226527e0f46e4c2a724b9
Harsh-2420 <harsh2420@gmail.com>
2021-04-06 05:23:09 +0530

    further work on images and test data

--- app.py
+++ app.py
@@ -4,36 +4,39 @@ import tensorflow as tf
 from tensorflow import keras
 import os
 from werkzeug.utils import secure_filename
+from keras.preprocessing.image import load_img, img_to_array, array_to_img, save_img
 
-# # Define Flask app
-# app = Flask(__name__)
+# Define Flask app
+app = Flask(__name__)
 
-# # Load the trained model
-# model = keras.models.load_model(
-#     '/Users/harshjhunjhunwala/Desktop/github/facial_keypoint/model.h5')
-
-
-# def model_predict(img_path, model):
-#     img = image.load()
+# Load the trained model
+model = keras.models.load_model(
+    '/Users/harshjhunjhunwala/Desktop/github/facial_keypoint/model.h5')
 
 
-# @app.route("/", methods=['GET'])
-# def index():
-#     return render_template('index.html')
+def model_predict(img_path, model):
+    img = load_img(img_path, target_size=(96, 96))
+    x = img_to_array(img)
 
 
-# @app.route("/predict", methods=['GET', "POST"])
-# def upload():
-#     if request.method == "POST":
-#         # Get the file from post request
-#         f = request.files['file']
-
-#         # Save the file to ./uploads
-#         basepath = os.path.dirname(__file__)
-#         filepath = os.path.join(basepath, 'uploads', secure_filename(f.filename))
-#         f.save(filepath)
-#         return 'file uploaded successfully'
+@app.route("/", methods=['GET'])
+def index():
+    return render_template('index.html')
 
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route("/predict", methods=['GET', "POST"])
+def upload():
+    if request.method == "POST":
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        filepath = os.path.join(basepath, 'uploads',
+                                secure_filename(f.filename))
+        f.save(filepath)
+        return 'file uploaded successfully'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- image_converter.py
+++ image_converter.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from keras.preprocessing.image import load_img, img_to_array
 
 
 original_array = np.loadtxt("images.txt").reshape(96, 96, 1)
 plt.imshow(original_array.reshape(96, 96), cmap='gray')
-plt.savefig('image_1')
+plt.savefig('image_2')
