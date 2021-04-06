7a7227d539a8f5701d9ccf11d1b3569268981d69
Harsh-2420 <harsh2420@gmail.com>
2021-04-06 23:56:17 +0530

    flask updates

--- image_converter.py
+++ image_converter.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from keras.preprocessing.image import load_img, img_to_array
 
 
-original_array = np.loadtxt("images.txt").reshape(96, 96, 1)
-plt.imshow(original_array.reshape(96, 96), cmap='gray')
-plt.savefig('image_2')
+img = load_img('image_1.png', target_size=(96, 96))
+x = img_to_array(img)
+# original_array = np.loadtxt("images.txt").reshape(96, 96, 1)
+# plt.imshow(original_array.reshape(96, 96), cmap='gray')
+# plt.savefig('image_2')
