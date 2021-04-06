7dda65f30dcb9f0ec00e12941f8ccdeccd6657dd
Harsh-2420 <harsh2420@gmail.com>
2021-04-06 05:40:08 +0530

    Flask backend setup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, jsonify, request, url_for
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 import os
@@ -17,6 +18,13 @@ model = keras.models.load_model(
 def model_predict(img_path, model):
     img = load_img(img_path, target_size=(96, 96))
     x = img_to_array(img)
+    # x = preprocess_input(x)
+
+    preds = model.predict(x)
+
+    # plt.imshow(x.reshape(96,96 ),cmap='gray')
+    # plt.scatter(preds[0::2], preds[1::2])
+    # plt.show()
 
 
 @app.route("/", methods=['GET'])
@@ -24,7 +32,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/predict", methods=['GET', "POST"])
+@app.route("/predict", methods=['GET', 'POST'])
 def upload():
     if request.method == "POST":
         # Get the file from post request
@@ -32,10 +40,13 @@ def upload():
 
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
-        filepath = os.path.join(basepath, 'uploads',
-                                secure_filename(f.filename))
-        f.save(filepath)
-        return 'file uploaded successfully'
+        file_path = os.path.join(basepath, 'uploads',
+                                 secure_filename(f.filename))
+        f.save(file_path)
+
+        preds = model_predict(file_path, model)
+        return preds
+    return None
 
 
 if __name__ == '__main__':
